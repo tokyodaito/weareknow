784ce61cabf65310ba61c4f807cf8cfc0055db71
Bogdan <bogdanbogotov@gmail.com>
2023-04-27 14:30:34 +0300

    git init

---
+++ app/src/main/java/com/bogsnebes/weareknow/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bogsnebes.weareknow.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bogsnebes.weareknow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
