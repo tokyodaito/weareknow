3b4a5592fa3fd08b694b460843b0f37e679cec79
Bogdan <bogdanbogotov@gmail.com>
2023-04-28 03:38:46 +0300

    create a bottomNavigationMenu

--- app/src/main/java/com/bogsnebes/weareknow/ui/MainActivity.kt
+++ app/src/main/java/com/bogsnebes/weareknow/ui/MainActivity.kt
@@ -4,12 +4,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bogsnebes.weareknow.R
 import com.bogsnebes.weareknow.ui.main_menu.MainMenuFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val mainMenuFragment by lazy { MainMenuFragment.newInstance() }
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private val bottomNavigationListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.firstFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, mainMenuFragment).commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.secondFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, mainMenuFragment).commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null) {
             supportFragmentManager
@@ -17,5 +40,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 .add(R.id.fragment_container, mainMenuFragment)
                 .commit()
         }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationListener)
     }
+
+
 }
