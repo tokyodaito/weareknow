9e583fa22ef667aab7a848a1c9ceee176e80ea9d
Eremeev Vlad <eremeev.vlad.v@yandex.ru>
2023-04-28 16:07:51 +0300

    Заготовка для жестов

--- app/src/main/java/com/bogsnebes/weareknow/accessibility/MyAccessibilityService.kt
+++ app/src/main/java/com/bogsnebes/weareknow/accessibility/MyAccessibilityService.kt
@@ -1,13 +1,21 @@
 package com.bogsnebes.weareknow.accessibility
 
+import android.accessibilityservice.AccessibilityGestureEvent
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 import com.bogsnebes.weareknow.accessibility.utils.EventProcessor
+import com.bogsnebes.weareknow.gestures.GestureProcessor
 
 class MyAccessibilityService : AccessibilityService() {
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
 //        println("Event: ${event.eventType}")
         EventProcessor.process(event, this, applicationContext)
+    }
+
+    override fun onGesture(gestureEvent: AccessibilityGestureEvent): Boolean {
+        println("Event: ${gestureEvent}")
+        GestureProcessor.process(gestureEvent, this, applicationContext)
+        return true
     }
 
     override fun onInterrupt() {
---
+++ app/src/main/java/com/bogsnebes/weareknow/gestures/GesturePArser.kt
@@ -0,0 +1,11 @@
+package com.bogsnebes.weareknow.gestures
+
+import android.accessibilityservice.AccessibilityGestureEvent
+import android.accessibilityservice.AccessibilityService
+import android.content.Context
+
+object GestureProcessor {
+    fun process(event: AccessibilityGestureEvent, service: AccessibilityService, context: Context) {
+
+    }
+}
