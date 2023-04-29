9539c8db75f4028c8664b913ed730ace79e418db
Bogdan <bogdanbogotov@gmail.com>
2023-04-29 03:15:14 +0300

    add new event types

--- app/src/main/java/com/bogsnebes/weareknow/accessibility/action/ActionConst.kt
+++ app/src/main/java/com/bogsnebes/weareknow/accessibility/action/ActionConst.kt
@@ -1,6 +1,6 @@
 package com.bogsnebes.weareknow.accessibility.action
 
-object ActionConst{
+object ActionConst {
     const val TIMEUNIT_SEC = "сек."
 }
 
@@ -24,4 +24,27 @@ object ActionObject {
     const val ELEM = "элемент"
     const val BUTTON = "кнопку"
     const val VIEW = "окно"
+}
+
+object EventTypes {
+    const val ANNOUNCEMENT = "объявление"
+    const val ASSIST_READING_CONTEXT = "помощь в чтении контекста"
+    const val GESTURE_DETECTION_END = "определение жестов окончание"
+    const val GESTURE_DETECTION_START = "определение жестов начало"
+    const val NOTIFICATION_STATE_CHANGED = "состояние уведомлений изменено"
+    const val SPEECH_STATE_CHANGE = "состояние речи изменено"
+    const val TOUCH_EXPLORATION_GESTURE_END = "исследование жестов конец"
+    const val TOUCH_EXPLORATION_GESTURE_START = "исследование жестов начало"
+    const val TOUCH_INTERACTION_END = "взаимодействие касанием конец"
+    const val TOUCH_INTERACTION_START = "взаимодействие касанием начало"
+    const val VIEW_ACCESSIBILITY_FOCUSED = "доступность фокусировки окна"
+    const val VIEW_ACCESSIBILITY_FOCUS_CLEARED = "очистка фокуса доступности окна"
+    const val VIEW_CONTEXT_CLICKED = "контекстное щелчком окна"
+    const val VIEW_FOCUSED = "фокусировка окна"
+    const val VIEW_HOVER_ENTER = "поднесение курсора над окном"
+    const val VIEW_HOVER_EXIT = "убирание курсора из окна"
+    const val VIEW_TEXT_CHANGED = "изменение текста окна"
+    const val VIEW_TEXT_SELECTION_CHANGED = "изменение выделения текста окна"
+    const val VIEW_TEXT_TRAVERSED = "перемещение по тексту окна"
+    const val WINDOWS_CHANGED = "изменение окон"
 }
--- app/src/main/java/com/bogsnebes/weareknow/accessibility/utils/EventProcessor.kt
+++ app/src/main/java/com/bogsnebes/weareknow/accessibility/utils/EventProcessor.kt
@@ -19,6 +19,26 @@ import com.bogsnebes.weareknow.accessibility.action.ActionType.CLICK
 import com.bogsnebes.weareknow.accessibility.action.ActionType.MOVE
 import com.bogsnebes.weareknow.accessibility.action.ActionType.OPEN
 import com.bogsnebes.weareknow.accessibility.action.ActionType.SCROLL
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.ANNOUNCEMENT
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.ASSIST_READING_CONTEXT
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.GESTURE_DETECTION_END
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.GESTURE_DETECTION_START
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.NOTIFICATION_STATE_CHANGED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.SPEECH_STATE_CHANGE
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.TOUCH_EXPLORATION_GESTURE_END
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.TOUCH_EXPLORATION_GESTURE_START
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.TOUCH_INTERACTION_END
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.TOUCH_INTERACTION_START
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_ACCESSIBILITY_FOCUSED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_ACCESSIBILITY_FOCUS_CLEARED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_CONTEXT_CLICKED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_FOCUSED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_HOVER_ENTER
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_HOVER_EXIT
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_TEXT_CHANGED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_TEXT_SELECTION_CHANGED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.VIEW_TEXT_TRAVERSED
+import com.bogsnebes.weareknow.accessibility.action.EventTypes.WINDOWS_CHANGED
 import com.bogsnebes.weareknow.accessibility.utils.AcsUtils.mkRect
 import com.bogsnebes.weareknow.common.StatusSaver
 import kotlinx.coroutines.launch
@@ -42,23 +62,121 @@ object EventProcessor {
                 SimpleWorker.executeInBackground {
                     if (!StatusSaver.IS_ON) {
                         when (event.eventType) {
-                            TYPE_VIEW_CLICKED, TYPE_VIEW_LONG_CLICKED -> processClick(
-                                event,
-                                context
-                            )
-                            WINDOWS_CHANGE_ACTIVE, WINDOWS_CHANGE_BOUNDS -> processWindow(
-                                event,
-                                service,
-                                context
-                            )
-                            TYPE_VIEW_SCROLLED -> processScroll(event, context)
+                            TYPE_VIEW_CLICKED -> processClick(event, context)
+                            TYPE_VIEW_LONG_CLICKED -> processLongClick(event, context)
+                            TYPE_VIEW_SCROLLED -> launch { processScroll(event, context) }
                             TYPE_VIEW_SELECTED -> processSelecting(event, context)
+                            WINDOWS_CHANGE_ACTIVE, WINDOWS_CHANGE_BOUNDS -> processWindow(event, service, context)
+                            TYPE_ANNOUNCEMENT -> processAnnouncement(event, context)
+                            TYPE_ASSIST_READING_CONTEXT -> processAssistReadingContext(event, context)
+                            TYPE_GESTURE_DETECTION_END -> processGestureDetectionEnd(event, context)
+                            TYPE_GESTURE_DETECTION_START -> processGestureDetectionStart(event, context)
+                            TYPE_NOTIFICATION_STATE_CHANGED -> processNotificationStateChanged(event, context)
+                            TYPE_SPEECH_STATE_CHANGE -> processSpeechStateChange(event, context)
+                            TYPE_TOUCH_EXPLORATION_GESTURE_END -> processTouchExplorationGestureEnd(event, context)
+                            TYPE_TOUCH_EXPLORATION_GESTURE_START -> processTouchExplorationGestureStart(event, context)
+                            TYPE_TOUCH_INTERACTION_END -> processTouchInteractionEnd(event, context)
+                            TYPE_TOUCH_INTERACTION_START -> processTouchInteractionStart(event, context)
+                            TYPE_VIEW_ACCESSIBILITY_FOCUSED -> processViewAccessibilityFocused(event, context)
+                            TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED -> processViewAccessibilityFocusCleared(event, context)
+                            TYPE_VIEW_CONTEXT_CLICKED -> processViewContextClicked(event, context)
+                            TYPE_VIEW_FOCUSED -> processViewFocused(event, context)
+                            TYPE_VIEW_HOVER_ENTER -> processViewHoverEnter(event, context)
+                            TYPE_VIEW_HOVER_EXIT -> processViewHoverExit(event, context)
+                            TYPE_VIEW_TEXT_CHANGED -> processViewTextChanged(event, context)
+                            TYPE_VIEW_TEXT_SELECTION_CHANGED -> processViewTextSelectionChanged(event, context)
+                            TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY -> processViewTextTraversed(event, context)
+                            TYPE_WINDOWS_CHANGED -> processWindowsChanged(event, context)
+                            else -> {
+                                // Handle unknown or unexpected event types here if needed
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    private fun processAnnouncement(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, ANNOUNCEMENT), context)
+    }
+
+    private fun processAssistReadingContext(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, ASSIST_READING_CONTEXT), context)
+    }
+
+    private fun processGestureDetectionEnd(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, GESTURE_DETECTION_END), context)
+    }
+
+    private fun processGestureDetectionStart(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, GESTURE_DETECTION_START), context)
+    }
+
+    private fun processNotificationStateChanged(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, NOTIFICATION_STATE_CHANGED), context)
+    }
+
+    private fun processSpeechStateChange(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, SPEECH_STATE_CHANGE), context)
+    }
+
+    private fun processTouchExplorationGestureEnd(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, TOUCH_EXPLORATION_GESTURE_END), context)
+    }
+
+    private fun processTouchExplorationGestureStart(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, TOUCH_EXPLORATION_GESTURE_START), context)
+    }
+
+    private fun processTouchInteractionEnd(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, TOUCH_INTERACTION_END), context)
+    }
+
+    private fun processTouchInteractionStart(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, TOUCH_INTERACTION_START), context)
+    }
+
+    private fun processViewAccessibilityFocused(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_ACCESSIBILITY_FOCUSED), context)
+    }
+
+    private fun processViewAccessibilityFocusCleared(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_ACCESSIBILITY_FOCUS_CLEARED), context)
+    }
+
+    private fun processViewContextClicked(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_CONTEXT_CLICKED), context)
+    }
+
+    private fun processViewFocused(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_FOCUSED), context)
+    }
+
+    private fun processViewHoverEnter(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_HOVER_ENTER), context)
+    }
+
+    private fun processViewHoverExit(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_HOVER_EXIT), context)
+    }
+
+    private fun processViewTextChanged(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_TEXT_CHANGED), context)
+    }
+
+    private fun processViewTextSelectionChanged(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_TEXT_SELECTION_CHANGED), context)
+    }
+
+    private fun processViewTextTraversed(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(USER, VIEW_TEXT_TRAVERSED), context)
+    }
+
+    private fun processWindowsChanged(event: AccessibilityEvent, context: Context) {
+        processAbstractNode(event, listOf(SYSTEM, WINDOWS_CHANGED), context)
+    }
+
 
     private fun processAbstractNode(
         event: AccessibilityEvent,
@@ -102,6 +220,11 @@ object EventProcessor {
     }
 
     private fun processClick(event: AccessibilityEvent, context: Context) {
+        val screen = AcsUtils.takePostScreenshot(context, mkRect(event))
+        processAbstractNode(event, listOf(USER, CLICK, ON), context, screen)
+    }
+
+    private fun processLongClick(event: AccessibilityEvent, context: Context) {
         val screen = AcsUtils.takePostScreenshot(context, mkRect(event))
         processAbstractNode(event, listOf(USER, CLICK, ON), context, screen)
     }
